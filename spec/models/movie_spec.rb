7b1ce0a6ad1c59ad7918924bf0445da44bb60c37
Jonathan Black <apps2black@gmail.com>
2014-07-31 15:25:26 -0700

    Added queries

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -12,4 +12,24 @@ describe "A movie" do
 
 		expect(movie.flop?).to be_false
 	end
+
+	it "is released when the released on date is in the past" do 
+		movie = Movie.create(movie_attributes(released_on: 3.months.ago))
+
+		expect(Movie.released).to include(movie)
+	end
+
+	it "is not relesed when the released on date is in the future" do
+		movie = Movie.create(movie_attributes(released_on: 3.months.ago))
+
+		expect(Movie.released).not_to include(movie)
+	end
+
+	it "released movies ordered with the most recently-released movie first" do
+		movie1 = Movie.create(movie_attributes(released_on: 3.months.ago))
+		movie2 = Movie.create(movie_attributes(released_on: 2.months.ago))
+		movie3 = Movie.create(movie_attributes(released_on: 1.month.ago))
+
+		expect(Movie.released).to eq([movie1, movie2, movie3])
+	end
 end
